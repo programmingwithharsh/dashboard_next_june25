5e9b0efbe5d9f76b319ed152bbbbaee170e0558f
Harsh Tiwari <harshtiwari343@email.com>
2025-06-09 11:21:33 +0530

    Added dummy products, bootstrap.js

---
+++ src/app/product-detail/page.js
@@ -0,0 +1,5 @@
+const ProductDetail = () => {
+    return (<h1>Product Detail Page</h1>);
+}
+
+export default ProductDetail;
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,11 +1,28 @@
-function ProductList({ products = [] }) {
-    return (<div>
-        <h1>This is Product List Functional Component</h1>
-        <ul>
-            {products.map((product, index) => (
-                <li key={index}>{product}</li>
+import defaultProducts from '../../data/Products';
+import Link from "next/link";
+
+function ProductList({ products = defaultProducts }) {
+    return (<div className='container mt-4'>
+        <h1 className='mb-4'>Product List</h1>
+        <div className='row'>
+            {products.map((product) => (
+                <div className='col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3' key={product.productId}>
+                    <div className="card h-100">
+                        <img src={product.imageUrl} style={{ height: '200px' }} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{product.productName}</h5>
+                            <p className="card-text"><strong>Code:</strong>{product.productCode}</p>
+                            <p className="card-text">{product.description}</p>
+                            <p className="card-text"><strong>Rating:</strong>{product.starRating}</p>
+                            <p className="card-text">{product.price}</p>
+                            <p className="card-text">Released: {product.releaseDate}</p>
+                            <Link className="btn btn-primary" href="/product-detail">Buy Now</Link>
+                        </div>
+                    </div>
+                </div>
             ))}
-        </ul>
+        </div>
+
     </div>
     );
 }
